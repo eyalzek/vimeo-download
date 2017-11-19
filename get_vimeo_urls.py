9d462fcc2292e80ef6277d299c09a97336b5c752
Eyal Zekaria <eyal.ez@gmail.com>
2017-11-19 15:13:22 +0100

    Add gui flow to the script
    in order to return results in a dict to the gui app

--- get_vimeo_urls.py
+++ get_vimeo_urls.py
@@ -40,11 +40,20 @@ def get_content_size(download_urls):
     return content_with_size
 
 
-def display_options(download_urls):
+def display_options(download_urls, gui=False):
     quality = ['High', 'Medium', 'Low'][-1 * len(download_urls):]
     options = sorted(get_content_size(download_urls),
                      key=lambda k: k['size'], reverse=True)
     l = len(download_urls) if len(download_urls) < 3 else 3
+    if gui:
+        links = []
+        for i in xrange(l):
+            links.append({
+                'quality': quality[i],
+                'size': options[i]['size'],
+                'url': options[i]['url']
+            })
+        return links
     for i in xrange(l):
         print('%s quality (%sMB):' % (quality[i], options[i]['size']))
         print('%s\n' % options[i]['url'])
@@ -59,6 +68,21 @@ def parse_args():
                         help='domain to specify as "Referer" (for private videos)')
     return parser.parse_args()
 
+def gui_flow(url):
+    results = {}
+    if 'vimeo' in url:
+        urls = [url]
+    else:
+        print('getting vimeo urls from page...\n')
+        urls = get_vimeo_links_from_page(url)
+        print('found %d vimeo videos' % len(urls))
+    referer = get_referer(url, None)
+    for url in urls:
+        print('getting download urls for %s\n' % url)
+        current_results = display_options(get_download_urls(url, referer), gui=True)
+        if len(current_results) > 0:
+            results[url] = current_results
+    return results
 
 def main():
     args = parse_args()
